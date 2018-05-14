902483eaa512103e011726048a4acfa5c0bfb103
Thao Vi <vitht@nustechnology.com>
2018-05-14 16:23:08 +0700

    add new mail function

--- app/controllers/my_mails_controller.rb
+++ app/controllers/my_mails_controller.rb
@@ -4,23 +4,35 @@ class MyMailsController < ApplicationController
 
   end
 
+  def new
+    @mail = MyMail.new
+  end
+
+  def create
+    object = User.find_by_email(params[:my_mail][:object_id])
+    @category = Category.find 2
+    @category.my_mails.create(user_id:current_user.id,subject: params[:my_mail][:subject],content:params[:my_mail][:content],object_id: object.id)
+
+  end
+
   def inbox
-    @inboxes = MyMail.where(category_id:1)
+    @inboxes = MyMail.where(category_id:2,object_id:current_user.id)
   end
 
   def sendbox
-    @send_boxes = MyMail.where(category_id:2)
+    @send_boxes = MyMail.where(category_id:2,user_id:current_user.id)
   end
 
   def draft
-    @drafts = MyMail.where(category_id:3)
+    @drafts = MyMail.where(category_id:3,user_id:current_user.id)
   end
 
   def read
-    @read = MyMail.where(category_id:1, is_readed:true)
+    @read = MyMail.where(category_id:1,is_readed:true,user_id:current_user.id)
   end
 
   def unread
-     @unread = MyMail.where(category_id:1, is_readed:false)
+     @unread = MyMail.where(category_id:1, is_readed:false,user_id:current_user.id)
   end
+
 end
--- app/models/my_mail.rb
+++ app/models/my_mail.rb
@@ -2,4 +2,5 @@ class MyMail < ApplicationRecord
   self.table_name = "mails"
   belongs_to :user
   belongs_to :category
+  belongs_to :receiver, foreign_key: 'object_id',class_name: "User"
 end
